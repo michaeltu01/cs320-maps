a53fc44f4aa383268fda30ef7db958f12e4473d1
Isaac Yi <isaacyi@isaacs-air.devices.brown.edu>
2023-11-06 23:41:05 -0500

    feat: got click to show the properties

--- front/src/WrappedMap.tsx
+++ front/src/WrappedMap.tsx
@@ -1,9 +1,9 @@
-import Map, { Layer, MapLayerMouseEvent, Source, ViewStateChangeEvent} from "react-map-gl";
+import Map, { Layer, MapLayerMouseEvent, Point, PointLike, Source, ViewStateChangeEvent} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import React from "react";
 import { ACCESS_TOKEN } from "./private/api";
 import { useState } from "react";
-import { FillLayer } from "react-map-gl";
+import { FillLayer, MapRef } from "react-map-gl";
 import { FeatureCollection } from "geojson";
 import { useEffect } from "react";
 import { overlayData } from "./overlays";
@@ -12,7 +12,7 @@ import { useRef } from "react";
 
 function WrappedMap() {
 
-  const mapRef = useRef(null); // Create a useRef to access the Map component
+  const mapRef = useRef<MapRef>(null)
 
     function onMapClick(e: MapLayerMouseEvent) {
         console.log(e.lngLat.lat);
@@ -22,16 +22,20 @@ function WrappedMap() {
         const map = mapRef.current;
 
         // Create a bounding box around the click point
-        const bbox = [
-          [e.point.x - 5, e.point.y - 5], 
-          [e.point.x + 5, e.point.y + 5] 
-        ];
+        const bbox: [PointLike, PointLike] = [
+          [e.point.x, e.point.y], 
+          [e.point.x, e.point.y] 
+        ]
 
         // Use the mapRef to call queryRenderedFeatures
-        // const features = map.queryRenderedFeatures(bbox);
-
-        // Now you can work with the features, for example, log them to the console
-        // console.log('Clicked features:', features);
+        if (map === null) {
+          console.error(new Error("map is empty"))
+        }
+        else {
+          const features = map.queryRenderedFeatures(bbox);
+          console.log('Clicked features:', features);
+        }
+        
       };
 
 
@@ -58,6 +62,7 @@ function WrappedMap() {
         style={{ width: window.innerWidth, height: window.innerHeight }}
         mapStyle={"mapbox://styles/mapbox/streets-v12"}
         onClick={(ev: MapLayerMouseEvent) => onMapClick(ev)}
+        ref={mapRef}
         >
         <Source id="geo_data" type="geojson" data={overlay}>
         <Layer {...geoLayer} />

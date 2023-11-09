5ae15a7a60e3137fd12d367ee073c0dadf9d1453
michaeltu01 <michaeltu705@gmail.com>
2023-11-08 20:36:26 -0500

    feat: added fetch call to retrieve GeoJson on the backend

--- front/src/WrappedMap.tsx
+++ front/src/WrappedMap.tsx
@@ -1,7 +1,14 @@
-import Map, { Layer, MapLayerMouseEvent, Point, PointLike, Source, ViewStateChangeEvent} from "react-map-gl";
+import Map, {
+  Layer,
+  MapLayerMouseEvent,
+  Point,
+  PointLike,
+  Source,
+  ViewStateChangeEvent,
+} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import React from "react";
-import { ACCESS_TOKEN } from "./private/api";
+import { ACCESS_TOKEN } from "./private/key";
 import { useState } from "react";
 import { FillLayer, MapRef } from "react-map-gl";
 import { FeatureCollection } from "geojson";
@@ -11,84 +18,89 @@ import { geoLayer } from "./overlays";
 import { useRef } from "react";
 
 function WrappedMap() {
+  const mapRef = useRef<MapRef>(null);
 
-    const mapRef = useRef<MapRef>(null)
+  const [viewState, setViewState] = useState({
+    longitude: -71.418884,
+    latitude: 41.825226,
+    zoom: 10,
+  });
 
+  const [overlay, setOverlay] = useState<GeoJSON.FeatureCollection | undefined>(
+    undefined
+  );
+  const [lastLog, setLastLog] = useState<string>(""); // To store and display the last console log
 
-    const [viewState, setViewState] = useState({
-        longitude: -71.418884,
-        latitude: 41.825226,
-        zoom: 10,
-    });
+  useEffect(() => {
+    async function fetchGeoJson() {
+      let geojson = undefined;
+      const fetchCall = await overlayData().then(
+        (featureCollection) => (geojson = featureCollection)
+      );
+      setOverlay(geojson);
+    }
+    fetchGeoJson();
+  }, []);
 
+  function onMapClick(e: MapLayerMouseEvent) {
+    console.log(e.lngLat.lat);
+    console.log(e.lngLat.lng);
 
-    const [overlay, setOverlay] = useState<GeoJSON.FeatureCollection | undefined>(undefined);
-    const [lastLog, setLastLog] = useState<string>(""); // To store and display the last console log
+    // Access the Mapbox component using mapRef
+    const map = mapRef.current;
 
+    // Create a bounding box around the click point
+    const bbox: [PointLike, PointLike] = [
+      [e.point.x, e.point.y],
+      [e.point.x, e.point.y],
+    ];
 
-    useEffect(() => {
-      setOverlay(overlayData());
-    }, []);
+    // Use the mapRef to call queryRenderedFeatures
+    if (map === null) {
+      console.error(new Error("map is empty"));
+    } else {
+      const features = map.queryRenderedFeatures(bbox);
+      const click_feature = features[0];
+      const properties = click_feature.properties;
 
-    function onMapClick(e: MapLayerMouseEvent) {
-      console.log(e.lngLat.lat);
-      console.log(e.lngLat.lng);
-
-      // Access the Mapbox component using mapRef
-      const map = mapRef.current;
-
-      // Create a bounding box around the click point
-      const bbox: [PointLike, PointLike] = [
-        [e.point.x, e.point.y], 
-        [e.point.x, e.point.y] 
-      ]
-
-      // Use the mapRef to call queryRenderedFeatures
-      if (map === null) {
-        console.error(new Error("map is empty"))
-      }
-      else {
-        const features = map.queryRenderedFeatures(bbox);
-        const click_feature = features[0];
-        const properties = click_feature.properties;
-
-        if (properties === null) {
-          console.log("properties are null")
-        }
-        else if (properties.city === undefined && properties.state === undefined && properties.name === undefined && properties.holc_grade === undefined) {
-          setLastLog("No data defined in click region. Try again")
-        }
-        else {
-          const logEntry = `state: ${properties.state}, city: ${properties.city}, holc_grade: ${properties.holc_grade} \n name: ${properties.name}`;
-          console.log(logEntry);
-          setLastLog(logEntry);
-        }
+      if (properties === null) {
+        console.log("properties are null");
+      } else if (
+        properties.city === undefined &&
+        properties.state === undefined &&
+        properties.name === undefined &&
+        properties.holc_grade === undefined
+      ) {
+        setLastLog("No data defined in click region. Try again");
+      } else {
+        const logEntry = `state: ${properties.state}, city: ${properties.city}, holc_grade: ${properties.holc_grade} \n name: ${properties.name}`;
+        console.log(logEntry);
+        setLastLog(logEntry);
       }
     }
-    
+  }
+
   return (
     <div>
-
-    <Map
+      <Map
         id="mapbox"
-        mapboxAccessToken = {ACCESS_TOKEN}
+        mapboxAccessToken={ACCESS_TOKEN}
         {...viewState}
         onMove={(ev: ViewStateChangeEvent) => setViewState(ev.viewState)}
         style={{ width: "100%", height: "100%" }}
         mapStyle={"mapbox://styles/mapbox/streets-v12"}
         onClick={(ev: MapLayerMouseEvent) => onMapClick(ev)}
         ref={mapRef}
-        >
+      >
         <Source id="geo_data" type="geojson" data={overlay}>
-        <Layer {...geoLayer} />
-      </Source>
-
-    </Map>
-    <div className="logs">
-      <h2>Click</h2>
-      <pre>{lastLog}</pre>
+          <Layer {...geoLayer} />
+        </Source>
+      </Map>
+      <div className="logs">
+        <h2>Click</h2>
+        <pre>{lastLog}</pre>
+      </div>
     </div>
-    </div>
-    )
+  );
 }
 export default WrappedMap;
